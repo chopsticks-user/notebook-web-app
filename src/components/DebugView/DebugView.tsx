8dbf2d05b1ab367eb63d073e8201947714bb4031
Frost <huuquangcap@gmail.com>
2022-07-27 02:24:41 -0400

    Added debugsection elemnents, feature elements

--- src/components/DebugView/DebugView.tsx
+++ src/components/DebugView/DebugView.tsx
@@ -1,7 +1,7 @@
-import React, { createRef, useContext } from "react"
-import "./DebugView.css"
+import React, { ReactElement, useContext } from "react"
 import { ThemeContext } from "../../App"
 import Split from "react-split"
+import "./DebugView.css"
 
 type DebugViewProps = {
   debugInfo: any;
@@ -12,36 +12,41 @@ const DebugView: React.FC<DebugViewProps> = ({ debugInfo }) => {
 
   return (
     <Split gutterSize={1} minSize={25} direction="vertical"
-      className="debug-view-container" style={{ color: theme.textColor }}>
-      <div className="debug-view-section-container" id="variables">
-        <header className="debug-view-section-header">
-          <div className="dropdown-icon-arrow">{">"}</div>
-          <b>VARIABLES</b>
-        </header>
-        <div className="debug-view-section-content">
-          {debugInfo}
-        </div>
-      </div>
-      <div className="debug-view-section-container" id="watch-list">
-        <header className="debug-view-section-header">
-          <div className="dropdown-icon-arrow">{">"}</div>
-          <b>WATCH LIST</b>
-        </header>
-        <div className="debug-view-section-content">
-
-        </div>
-      </div>
-      <div className="debug-view-section-container" id="timeline">
-        <header className="debug-view-section-header">
-          <div className="dropdown-icon-arrow">{">"}</div>
-          <b>TIMELINE</b>
-        </header>
-        <div className="debug-view-section-content">
-
-        </div>
-      </div>
+      className="debug-view-container"
+      style={{ color: theme.textColor }}
+    >
+      <DebugSection domElementID="variables" sectionName="VARIABLES" feature={<Feature />} />
+      <DebugSection domElementID="watch-list" sectionName="WATCH LIST" feature={<Feature />} />
+      <DebugSection domElementID="timeline" sectionName="TIMELINE" feature={<Feature />} />
     </Split>
-  )
+  );
+}
+
+type DebugSectionProps = {
+  domElementID: string;
+  sectionName: string;
+  feature: ReactElement<any>;
+}
+
+const DebugSection: React.FC<DebugSectionProps> = ({ domElementID, sectionName, feature }) => {
+  const dropDownIcon: string = ">";
+  return (
+    <div className="debug-view-section-container"
+      id={domElementID}
+    >
+      <header className="debug-view-section-header">
+        <div className="dropdown-icon-arrow">{dropDownIcon}</div>
+        <b>{sectionName}</b>
+      </header>
+      <div className="debug-view-section-content">
+        {feature}
+      </div>
+    </div>
+  );
+}
+
+const Feature = () => {
+  return <span></span>;
 }
 
 export default DebugView;
